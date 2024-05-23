721c7a92f758c109e43d526e8e1b4be5b34fc18c
Danila Novitsky <120642419+DanilaNovitscky@users.noreply.github.com>
2024-05-23 12:22:57 +0300

    Add files via upload

---
+++ Дроби/kras_2/kras_2/kras_2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+struct Rational {
+    int numerator;
+    int denominator;
+
+    Rational(int n, int d) {
+        int g = gcd(n, d);
+        numerator = n / g;
+        denominator = d / g;
+    }
+
+    int gcd(int a, int b) {
+        return (b == 0) ? a : gcd(b, a % b);
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    Rational myRational(6, 9);
+    std::cout << "Числитель: " << myRational.numerator << std::endl;
+    std::cout << "Знаменатель: " << myRational.denominator << std::endl;
+
+    return 0;
+}
---
+++ Чайник/chainik/chainik/chainik.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+using namespace std;
+
+class Teapot {
+private:
+	bool isFilled;
+	bool isOnStove;
+	bool isGasLit;
+
+public:
+	// Конструктор с параметрами, позволяющий инициализировать начальное состояние чайника
+	Teapot(bool filled, bool onStove, bool gasLit) : isFilled(filled), isOnStove(onStove), isGasLit(gasLit) {}
+
+	void fillWithWater() {
+		cout << "Чайник наполняется водой...\n";
+		isFilled = true;
+	}
+
+	void placeOnStove() {
+		cout << "Чайник ставится на плиту...\n";
+		isOnStove = true;
+	}
+
+	void lightGas() {
+		cout << "Газовая плита зажигается...\n";
+		isGasLit = true;
+	}
+
+	void waitUntilBoiling() {
+		cout << "Ждем, пока вода в чайнике закипит...\n";
+		for (int i = 0; i < 3; i++) {
+			cout << "Происходит нагрев...\n";
+		}
+		cout << "Чайник закипел!\n";
+	}
+
+	void makeTea() {
+		if (!isFilled) {
+			fillWithWater();
+		}
+		if (!isOnStove) {
+			placeOnStove();
+		}
+		if (!isGasLit) {
+			lightGas();
+		}
+		waitUntilBoiling();
+	}
+};
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	cout << "Стоит ли чайник с водой уже на плите? (1 - да, 0 - нет): ";
+	bool isOnStove;
+	cin >> isOnStove;
+
+	// Создаем объект класса Teapot в зависимости от ответа пользователя
+	if (isOnStove) {
+		// Если чайник уже на плите, создаем объект Teapot с нужными параметрами
+		Teapot teapot(true, true, false);
+		teapot.makeTea();
+	}
+	else {
+		// Если чайник не на плите, создаем объект Teapot с начальными параметрами
+		Teapot teapot(false, false, false);
+		teapot.makeTea();
+	}
+
+	return 0;
+}
