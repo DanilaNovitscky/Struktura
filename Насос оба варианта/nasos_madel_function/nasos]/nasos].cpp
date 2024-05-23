91d977b954b20998e463de6be07d0b33ddd26673
Danila Novitsky <120642419+DanilaNovitscky@users.noreply.github.com>
2024-05-23 16:02:01 +0300

    Add files via upload

---
+++ Насос оба варианта/nasos_madel_function/nasos]/nasos].cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <cmath>
+
+double nas_hr(double a);
+double nas_hc(double a);
+double nas_kr(double a);
+double nas_kc(double a);
+
+double nas(double (*h_func)(double), double (*k_func)(double), double h, double k) {
+    return h_func(h) * 0.5 * exp(k_func(k));
+}
+
+double nas_hr(double a) {
+    return a * 12.5;
+}
+
+double nas_hc(double a) {
+    return a * 10.32;
+}
+
+double nas_kr(double a) {
+    return exp(a);
+}
+
+double nas_kc(double a) {
+    return exp(1.2 * a);
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    double h_arg = 5, k_arg = 1.23;
+    std::cout << "Рассчитанный параметр Р для r: " << nas(nas_hr, nas_kr, h_arg, k_arg) << std::endl;
+    std::cout << "Рассчитанный параметр Р для c: " << nas(nas_hc, nas_kc, h_arg, k_arg) << std::endl;
+
+    return 0;
+}
---
+++ Насос оба варианта/nasos_model_classov/kras_kr/kras_kr.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#include <cmath>
+#include <functional>
+
+using namespace std;
+
+// Определение абстрактной функции насоса
+using NasFunction = function<double(double)>;
+
+// Определение типа функции для вычисления КПД и H для роторного и центробежного насосов
+using EfficiencyFunction = function<double(double)>;
+
+// Функция для моделирования роторного насоса
+double rotaryPump(double flowRate) {
+    // Пример реализации функции мощности роторного насоса
+    return pow(flowRate, 2); // Мощность растет пропорционально квадрату расхода
+}
+
+// Функция для моделирования центробежного насоса
+double centrifugalPump(double flowRate) {
+    // Пример реализации функции мощности центробежного насоса
+    return pow(flowRate, 2.5); // Мощность растет более быстро
+}
+
+// Функция для вычисления КПД роторного насоса
+double efficiencyRotary(double flowRate) {
+    // Пример реализации функции КПД роторного насоса
+    return 1.0 / (1.0 + 0.1 * flowRate); // КПД уменьшается по мере роста расхода
+}
+
+// Функция для вычисления КПД центробежного насоса
+double efficiencyCentrifugal(double flowRate) {
+    // Пример реализации функции КПД центробежного насоса
+    return 1.0 / (1.0 + 0.05 * flowRate); // КПД уменьшается, но не так быстро как у роторного насоса
+}
+
+// Функция для вычисления параметров роторного насоса
+double Nas_r(double flowRate) {
+    return rotaryPump(flowRate) * efficiencyRotary(flowRate);
+}
+
+// Функция для вычисления параметров центробежного насоса
+double Nas_c(double flowRate) {
+    return centrifugalPump(flowRate) * efficiencyCentrifugal(flowRate);
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    // Ввод значения расхода через консоль
+    double flowRate;
+    cout << "Введите значение расхода: ";
+    cin >> flowRate;
+
+    // Выбор типа насоса в зависимости от переданных функций
+    NasFunction nas;
+
+    // Выбор функции для вычисления параметров насоса
+    cout << "Выберите тип насоса:" << endl;
+    cout << "1. Роторный насос" << endl;
+    cout << "2. Центробежный насос" << endl;
+
+    int choice;
+    cin >> choice;
+
+    switch (choice) {
+    case 1:
+        nas = Nas_r;
+        break;
+    case 2:
+        nas = Nas_c;
+        break;
+    default:
+        cerr << "Неверный выбор." << endl;
+        return 1;
+    }
+
+    // Расчет параметров насоса
+    double power = nas(flowRate);
+
+    // Вывод результата
+    cout << "Мощность насоса: " << power << endl;
+
+    return 0;
+}
